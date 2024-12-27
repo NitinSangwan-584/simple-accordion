1f5898564121a9e68b11026f7a8bb19d03f342a4
NitinSangwan-584 <nitinsangwan363@gmail.com>
2024-12-27 10:35:30 +0530

    style css change

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const accordionHeaders = document.querySelectorAll(".accordion-header");
-  
-    accordionHeaders.forEach(header => {
-      header.addEventListener("click", () => {
-        const currentContent = header.nextElementSibling;
-  
-        // Close all other accordion sections
-        document.querySelectorAll(".accordion-content").forEach(content => {
-          if (content !== currentContent) {
-            content.style.display = "none";
-          }
-        });
-  
-        // Toggle the current accordion section
-        currentContent.style.display =
-          currentContent.style.display === "block" ? "none" : "block";
+  const accordionHeaders = document.querySelectorAll(".accordion-header");
+  accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+      const currentContent = header.nextElementSibling;
+      document.querySelectorAll(".accordion-content").forEach(content => {
+        if (content !== currentContent) {
+          content.style.display = "none";
+        }
       });
+      currentContent.style.display =
+        currentContent.style.display === "block" ? "none" : "block";
     });
   });
-  
+});
